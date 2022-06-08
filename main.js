56e3f4a5cf6c1f568bd353c964b352a3efa5b225
vijaydev133 <vijaydeveloper004@gmail.com>
2022-06-08 09:45:20 +0530

    Adding code.

---
+++ main.js
@@ -0,0 +1,84 @@
+/********************************************** */
+//Function 
+
+// let num1 = parseFloat(prompt("enter num1: "))
+
+// let num2 = parseFloat(prompt("enter num2: "))
+
+// let operator = prompt("Enter operator (ex. +, -, *, /)" );
+
+
+// function operation(x, y, op){
+//     if(op == "+"){
+//         console.log(x + y);
+//     }
+//     else if(op == "-"){
+//         console.log(x - y);
+//     }
+//     else if(op == "*"){
+//         console.log(x * y);
+//     }
+//     else if(op == "/"){
+//         console.log(x / y);
+//     }
+//     else{
+//         console.log("Enter valid operator");
+//     }
+// }
+
+// operation(num1,num2,operator);
+
+/*************************************************** */
+
+// let num = parseInt(prompt("Enter the number to be reversed:"))
+// let rev = 0;
+// let rem = 0;
+// function reverseNum(x){
+//     while(x > 0){
+//         rem = x % 10;
+//         rev = (rev * 10) + rem;
+//         x = Math.floor(x / 10)
+//     }
+//     return rev;
+// }
+
+// let result = reverseNum(num);
+// console.log(result);
+
+/*************************************************** */
+
+// let num = parseInt(prompt("Enter the number :"))
+
+// function oddEven(x){
+//     if(x % 2 == 0){
+//         console.log("the number is even");
+//     }
+//     else{
+//         console.log("the number is odd");
+//     }
+// }
+
+// oddEven(num);
+
+/*************************************************** */
+
+// let mark = parseInt(prompt("Enter your mark :"))
+
+// function grade(x){
+//     if(mark <= 40){
+//         console.log("you got D grade");
+//     }
+//     else if(mark > 40 && mark <= 60){
+//         console.log("you got C grade");
+//     }
+//     else if(mark > 60 && mark <= 80){
+//         console.log("you got B grade");
+//     }
+//     else if(mark > 80 && mark <= 100){
+//         console.log("you got A grade");
+//     }
+//     else{
+//         console.log("enter mark");
+//     }
+// }
+// grade(mark);
